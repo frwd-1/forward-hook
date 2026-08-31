51621da0061eef7a310220b8f0b159082db4c717
andrew <andrew@frwdlabs.xyz>
2026-08-31 18:45:53 +0000

    demo: init case study

--- src/PoolStateReader.sol
+++ src/PoolStateReader.sol
@@ -1,17 +1,4 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.26;
-
-// import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-// import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
-// import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
-// import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
-// import {BaseHook} from "@openzeppelin/uniswap-hooks/src/base/BaseHook.sol";
-
-// contract PoolStateReader is BaseHook {
-//     using PoolIdLibrary for PoolKey;
-//     using StateLibrary for IPoolManager;
-
-//     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
-//         poolManager = _poolManager;
-//     }
-// }
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+// tx hash:
+// 0x2521bf732e6c502d3047bebda665b291b054d7d3e60eeacb90403a947cba7519
